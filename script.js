0c101781cd30987a6c3667c096e426c2284a3bd6
Himani Kumari <231017332+himanii1218-sketch@users.noreply.github.com>
2026-06-06 22:21:45 +0530

    Add files via upload
    Added dynamic QR code generation feature

--- script.js
+++ script.js
@@ -3,14 +3,34 @@ let attendance = [];
 function markAttendance() {
     let roll = document.getElementById("roll").value;
 
-    let now = new Date();
+    if (roll === "") {
+        alert("Enter Roll Number");
+        return;
+    }
 
     attendance.push(
-        roll + " - " + now.toLocaleString()
+        roll + " - " + new Date().toLocaleString()
     );
 
     document.getElementById("result").innerHTML =
         attendance.join("<br>");
 
     document.getElementById("roll").value = "";
+}
+
+function generateQR() {
+    let roll = document.getElementById("roll").value;
+
+    if (roll === "") {
+        alert("Enter Roll Number");
+        return;
+    }
+
+    document.getElementById("qrcode").innerHTML = "";
+
+    new QRCode(document.getElementById("qrcode"), {
+        text: roll,
+        width: 200,
+        height: 200
+    });
 }
